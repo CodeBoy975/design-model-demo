942785de87398d2d833a50495db4bb9d3d1c06de
CodeBoy975 <986736558@qq.com>
2020-01-07 17:18:49 +0800

    接口统计信息设计

---
+++ design-code/design-code-demo-25/src/main/java/com/yao/DesignCodeDemo25Application.java
@@ -0,0 +1,17 @@
+package com.yao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  接口统计信息开发
+ * @author pengjie_yao
+ */
+@SpringBootApplication
+public class DesignCodeDemo25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignCodeDemo25Application.class, args);
+    }
+
+}
